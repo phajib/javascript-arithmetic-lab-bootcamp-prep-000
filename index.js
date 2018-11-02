df533dbbeb30314e4dff748956fd1066ed1e0917
H B <phajib@gmail.com>
2018-11-02 05:53:41 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,54 @@
+// add function
+function add(a, b){
+return a + b
+}
+
+//subtract function
+function subtract(a, b){
+  return a - b
+}
+
+//multiply function
+function multiply(a, b){
+  return a * b
+}
+
+//divivde function
+function divide(a, b){
+  return a / b
+}
+
+//increment function
+function inc(n){
+  return n + 1
+}
+
+//decrements function
+function dec(n){
+  return n - 1
+}
+
+//makeInt function
+function makeInt(n){
+  parseInt(n)
+  return n
+  //makeInt(a.toString())
+  //return a
+
+  //makeInt('0x2328', 10)
+  //parseInt(0x2328)
+  //parseInt(n, 10)
+  return parseInt(n, 10)
+
+  isNan(makeInt('sldkjflksjf'))
+  return Nan
+}
+
+function preserveDecimal(n){
+  return parseFloat(n)
+  preserveDecimal('2.222')
+  return 2.222
+
+  isNan(preserveDecimal('sldkjflksjf'))
+  return Nan
+}
