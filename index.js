9e2e6a5fb37e57150242339af5be371ed2099a9e
H B <phajib@gmail.com>
2018-11-02 06:01:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -41,6 +41,6 @@ function preserveDecimal(n){
   return parseFloat(n)
   return preserveDecimal('2.222')
 
-  isNan(preserveDecimal('sldkjflksjf'))
+  isNan(parseFloat('sldkjflksjf'))
   return Nan
 }
