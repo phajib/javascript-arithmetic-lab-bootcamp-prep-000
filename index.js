7541d1b4a4ae9c8aff6c5f32aa61bec0956e9067
H B <phajib@gmail.com>
2018-11-02 06:07:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -40,8 +40,6 @@ function makeInt(n){
 //preserveDecimal function
 function preserveDecimal(n){
   return parseFloat(n)
-  //return preserveDecimal('2.222')
 
   isNan(parseInt(n))//('sldkjflksjf'))
-  //return Nan
 }
